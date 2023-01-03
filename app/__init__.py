3c7be9c68e60d8fd6eda83f1187f4813cd3a5ecd
annadu <duyuan0921@gmail.com>
2023-01-02 17:18:43 -0800

    import database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
+db = SQLAlchemy()
+migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
+
+
+    from .models.books import Book
+    from .models.authors import Author
 
     from .routes.route_books import books_bp
     app.register_blueprint(books_bp)
